06cd7c163897ab2723f9e6bc9cada8d3b5f9ad61
yuyang <yuyangl119@126.com>
2020-02-29 22:33:54 +0800

    NEW_BRANCH_STARTING_WITH_MODIFYING_USER_DATA

---
+++ src/views/user/Edit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="users-show">
+    <div class="col-md-3 main-col">
+      <div class="box">
+        <div class="padding-md">
+          <div class="list-group text-center">
+            <router-link v-for="link in links" :key="link.name" :to="`/users/1/${link.name}`" class="list-group-item">
+              <i :class="`text-md fa fa-${link.icon}`"></i>
+              {{ link.title }}
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserEdit"
+}
+</script>
+
+<style>
+
+</style>
